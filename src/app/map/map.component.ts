1b50cc7af0c17f59f08932c80f8e120f960c034a
jt4000 <johan.truong12@gmail.com>
2021-01-06 16:08:53 +0100

    .

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -18,8 +18,8 @@ export class MapComponent implements OnInit {
   private initMap(): void {
 
     // 58.035942, 14.97178
-
-
+    //   https://stackblitz.com/edit/jaime-leaflet-custom-markers?file=app%2Fapp.component.html
+    // https://stackoverflow.com/questions/55211396/how-to-add-marker-to-leaflet-map-in-angular-component/55211483#55211483
     this.map = L.map('map', {
       center: [39.8282, -98.5795],
       zoom: 5
@@ -29,10 +29,10 @@ export class MapComponent implements OnInit {
       iconUrl: 'marker-icon.png',
       shadowUrl: 'marker-shadow.png',
       iconSize: [25, 41],
-      iconAnchor: [25, 41]
+      iconAnchor: [13, 41],
     });
 
-    L.marker([51.5, -0.09]).addTo(this.map)
+    L.marker([51.5, -0.09], { icon: myIcon }).addTo(this.map)
       .bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
 
     L.marker([37.98, 23.72], { icon: myIcon })
@@ -45,8 +45,9 @@ export class MapComponent implements OnInit {
 
     tiles.addTo(this.map);
 
-    L.marker([51.5, -0.09], { icon: myIcon }).addTo(this.map)
-      .bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
+    L.marker([59.340214, 18.018026
+    ], { icon: myIcon }).addTo(this.map)
+      .bindPopup("<b>Hello world!</b><br />I am a popupp.").openPopup();
 
   }
 }
